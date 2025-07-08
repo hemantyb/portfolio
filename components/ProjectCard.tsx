c7177469b94255f46176bdb70f561d283fe2ea05
Hemant Baviskar <hemantyb22@gmail.com>
2025-07-08 19:56:18 +0530

    removed unused statement

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -7,7 +7,6 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 import { Project } from "@/types";
 
